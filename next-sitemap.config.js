e15aeda2f0368411c2b5c2ea7e4d45a811a50137
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2025-02-06 10:34:59 +0530

    chore: enable index sitemap generation and update sitemap entries

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://mustafalanewala.vercel.app",
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
